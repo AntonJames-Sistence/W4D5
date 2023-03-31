c37b2f01d55a59a7dddbae45e188bd88567e53cd
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-31 15:08:44 -0400

    Add stock_picker and my_transpose

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 def my_uniq(array)
     result = []
     array.each do |ele|
@@ -17,4 +17,40 @@ class Array
         end
         results
     end
+end
+
+def my_transpose(arr)
+    transposed = []
+    return transposed if arr.empty?
+
+    (0...arr[0].length).each do |j|
+        row = []
+        # debugger
+        (0...arr.length).each do |i|
+            row << arr[i][j]
+        end
+        transposed << row
+    end
+
+    transposed
+end
+
+def stock_picker(days)
+    max_profit = 0
+    days_idx = nil
+
+    days.each_with_index do |price1, day1|
+        days.each_with_index do |price2, day2|
+            if day1 < day2
+                difference = price2 - price1
+
+                if difference > max_profit
+                    max_profit = difference
+                    days_idx = [day1, day2]
+                end
+            end
+        end
+    end
+
+    days_idx
 end
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -27,3 +27,53 @@ describe "#two_sum" do
         expect(array.two_sum.length > 2).to_not be true
     end
 end
+
+describe '#my_transpose' do
+    array = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ]
+    array2 = [
+        [0, 1, 2]
+    ]
+
+    it 'Return trasposed array' do
+        expect(my_transpose(array)).to eq([
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ])
+    end
+
+    it "Shoud return a new array" do
+        expect(my_transpose(array)).to_not be array
+    end
+
+    it "Should work for any matrix even empty" do
+        expect(my_transpose(array2)).to eq([
+            [0],
+            [1],
+            [2]
+        ])
+
+        expect(my_transpose([])).to eq([])
+    end
+end
+
+describe '#stock_picker' do
+    arr = [5, 3, -7, 8, -9]
+
+    it "It should return a pair of days" do
+        expect(stock_picker(arr).length).to eq(2)
+    end
+
+    it "It shoul return a correct days" do
+        expect(stock_picker(arr)).to eq([2, 3])
+    end
+
+    it 'It should not sell stocks before day when you bought it' do
+        expect(stock_picker(arr)).to_not eq([4, 3])
+    end
+
+end
